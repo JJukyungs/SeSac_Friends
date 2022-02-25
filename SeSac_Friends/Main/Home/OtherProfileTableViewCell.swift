0ff633fe4a518d39b256c8fe7f3b7fc0a4299d38
JJukyungs <wnrud675@naver.com>
2022-02-25 16:30:36 +0900

    22.02.25  [ 새싹 찾기 뷰 갈아업기 시작] section 별로 나누어서 다른유저 정보를 가져오고 tableViewCell Extendable로 작동하게 한번 도전!!! 안되면 나도 몰?루?

--- SeSac_Friends/Main/Home/OtherProfileTableViewCell.swift
+++ SeSac_Friends/Main/Home/OtherProfileTableViewCell.swift
@@ -47,11 +47,6 @@ class OtherProfileTableViewCell: UITableViewCell {
         return view
     }()
     
-//    let subView: UIView = {
-//        let view = UIView()
-//        view.backgroundColor = .orange // 삭제해야함
-//        return view
-//    }()
     
     var subView = UIView()
     {
@@ -70,9 +65,35 @@ class OtherProfileTableViewCell: UITableViewCell {
     let requtationView = ReputationView()
     
     
-    @objc func toggleButtonClicked() {
-        toggleButtonAction?()
-    }
+    let hobbyTitle: UILabel = {
+        let label = UILabel()
+        label.text = "하고 싶은 취미"
+        label.font = UIFont.Title6_R12
+        return label
+    }()
+    
+    // 취미 컬렉션 뷰
+    let otherUserHobbyCollectionView: UICollectionView = {
+        let layout = LeftAlignedCollectionViewFlowLayout()
+        layout.minimumLineSpacing = 8
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.isScrollEnabled = false
+        collectionView.collectionViewLayout = layout
+        return collectionView
+    }()
+    
+    let reviewTitle: UILabel = {
+        let label = UILabel()
+        label.text = "새싹 리뷰"
+        label.font = UIFont.Title6_R12
+        return label
+    }()
+    
+    
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
@@ -80,10 +101,20 @@ class OtherProfileTableViewCell: UITableViewCell {
         setupConstraints()
     
         toggleView.arrowButton.addTarget(self, action: #selector(toggleButtonClicked), for: .touchUpInside)
+        
+        otherUserHobbyCollectionView.dataSource = self
+        otherUserHobbyCollectionView.delegate = self
+        
+        // HomeHobbyCollectionCell 재사용
+        otherUserHobbyCollectionView.register(HomeHobbyCollectionCell.self, forCellWithReuseIdentifier: HomeHobbyCollectionCell.identifier)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func toggleButtonClicked() {
+        toggleButtonAction?()
     }
     
     func setupView() {
@@ -93,6 +124,9 @@ class OtherProfileTableViewCell: UITableViewCell {
         stackview.addArrangedSubview(subView)
         subView.addSubview(sesacTitle)
         subView.addSubview(requtationView)
+        subView.addSubview(hobbyTitle)
+        subView.addSubview(otherUserHobbyCollectionView)
+        subView.addSubview(reviewTitle)
         
     }
     
@@ -127,7 +161,85 @@ class OtherProfileTableViewCell: UITableViewCell {
             make.top.equalTo(sesacTitle.snp.bottom).offset(16)
             make.leading.trailing.equalToSuperview().inset(16)
 //            make.height.equalTo(112)
-            make.bottom.equalToSuperview()
+            make.bottom.equalTo(hobbyTitle.snp.top).offset(-16)
+        }
+        hobbyTitle.snp.makeConstraints { make in
+            make.top.equalTo(requtationView.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(18)
+        }
+        otherUserHobbyCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(hobbyTitle.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(16)
+//            make.bottom.equalTo(reviewTitle.snp.top)
+            make.height.equalTo(150)
+        }
+        reviewTitle.snp.makeConstraints { make in
+            make.top.equalTo(otherUserHobbyCollectionView.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(16)
+//            make.bottom.equalToSuperview()
+            make.height.equalTo(18)
         }
     }
+}
+
+
+extension OtherProfileTableViewCell: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
+        return 1
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+       return 3
+        
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+      
+        guard let hobbyCell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeHobbyCollectionCell.identifier, for: indexPath) as? HomeHobbyCollectionCell else { return UICollectionViewCell() }
+            
+        hobbyCell.tagLabel.text = "testest"
+           
+            
+        return hobbyCell
+            
+        
+    }
+    
+   
+    
+    
+   
+    
+    
+    
+    
+}
+
+
+extension OtherProfileTableViewCell: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let textlabel: UILabel = {
+            let label = UILabel()
+//            label.text = viewModel.myHobbyArray.value[indexPath.item]
+            label.text = "testest"
+            label.font = UIFont.Title4_R14
+            label.sizeToFit()
+            return label
+        }()
+        let size = textlabel.frame.size
+        
+        return CGSize(width: size.width + 32, height: size.height + 10)
+       
+    }
+    
+    
 }
